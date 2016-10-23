c393c5315806b923915e802f1ddafb5ffb980eee
Wendy <wizhacks@github.com>
2016-10-23 14:15:20 -0400

    Set the constructor property of the child to itself.

--- app/js/util.js
+++ app/js/util.js
@@ -8,5 +8,6 @@
  * Child inherits from parent
  */
 function inherits(child, parent) {
-    child.prototype = Object.create(parent.prototype);
+    child.prototype = Object.create(parent.prototype);	
+	child.prototype.constructor = parent;
 }
