189b9f595c0f120fbb33e2d71e26d378ae9ebb9f
Brian Yang <brian@brianyang.me>
2016-10-24 00:34:21 -0400

    Add Materialize SCSS

--- gulpfile.js
+++ gulpfile.js
@@ -9,31 +9,37 @@ gulp.task('default', ['scripts', 'scripts:watch', 'serve', 'sass', 'sass:watch']
 
 // concatenate all our scripts
 gulp.task('scripts', function() {
-  return gulp.src([
-      './app/js/util.js',
-	  './app/js/screens/Screen.js',
-      './app/js/*/*.js',
-      './app/js/main.js'
-    ])
-    .pipe(concat('wisteria.js'))
-    .pipe(gulp.dest('./app/dist/js/'));
+    return gulp.src([
+            './app/js/util.js',
+            './app/js/screens/Screen.js',
+            './app/js/*/*.js',
+            './app/js/main.js'
+        ])
+        .pipe(concat('wisteria.js'))
+        .pipe(gulp.dest('./app/dist/js/'));
 });
 
 // automatically concat scripts on change
-gulp.task('scripts:watch', function () {
-  gulp.watch('./app/js/**/*.js', ['scripts']);
+gulp.task('scripts:watch', function() {
+    gulp.watch('./app/js/**/*.js', ['scripts']);
+});
+
+// copy materialize
+gulp.task('materialize', function() {
+    return gulp.src('./node_modules/materialize-css/sass/**/*.scss')
+        .pipe(gulp.dest('./app/sass/materialize'));
 });
 
 // compile scss files
-gulp.task('sass', function () {
-  return gulp.src('./app/sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./app/dist/css'));
+gulp.task('sass', ['materialize'], function() {
+    return gulp.src(['./app/sass/**/*.scss', '!./app/sass/materialize/*.scss'])
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./app/dist/css'));
 });
 
 // automatically recompile scss on change
-gulp.task('sass:watch', function () {
-  gulp.watch('./app/sass/**/*.scss', ['sass']);
+gulp.task('sass:watch', function() {
+    gulp.watch('./app/sass/**/*.scss', ['sass']);
 });
 
 // quick server
