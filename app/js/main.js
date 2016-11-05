bc56442ace5efa06ded11e1ffb108813fa277ad8
Brian Yang <brian@brianyang.me>
2016-11-05 18:17:04 -0400

    Remove unnecessary requires

--- app/js/main.js
+++ app/js/main.js
@@ -9,46 +9,6 @@
  * This is frontend only.
  */
 
-// include backend components
-var GameData = require('./backend/GameData');
-var GameLogicManager = require('./backend/GameLogicManager');
-var GameManager = require('./backend/GameManager');
-var Level = require('./backend/Level');
-var LevelManager = require('./backend/LevelManager');
-var Powerup = require('./backend/Powerup');
-var PowerupManager = require('./backend/PowerupManager');
-var ScreenManager = require('./backend/ScreenManager');
-var Shape = require('./backend/Shape');
-var ShapeManager = require('./backend/ShapeManager');
-var User = require('./backend/User');
-
-// include screens
-var Screen = require('./screens/Screen');
-var AboutScreen = require('./screens/AboutScreen');
-
-// include screens
-var Screen = require('./screens/Screen');
-var AboutScreen = require('./screens/AboutScreen');
-var AdScreen = require('./screens/AdScreen');
-var DefeatScreen = require('./screens/DefeatScreen');
-var GameMapScreen = require('./screens/GameMapScreen');
-var GameplayScreen = require('./screens/GameplayScreen');
-var LevelEditScreen = require('./screens/LevelEditScreen');
-var LevelSelectScreen = require('./screens/LevelSelectScreen');
-var LevelStoryScreen = require('./screens/LevelStoryScreen');
-var PauseScreen = require('./screens/PauseScreen');
-var PrivateCustomLevelsScreen = require('./screens/PrivateCustomLevelsScreen');
-var PublicCustomLevelsScreen = require('./screens/PublicCustomLevelsScreen');
-var SaveLevelScreen = require('./screens/SaveLevelScreen');
-var SplashScreen = require('./screens/SplashScreen');
-var StoreScreen = require('./screens/StoreScreen');
-var VictoryScreen = require('./screens/VictoryScreen');
-
-var DropdownMenu = require('./components/overflow');
-var UserMenu = require('./components/UserMenu');
-var PixiCanvas = require('./components/pixi-canvas');
-var Canvas = require('./components/canvas');
-
 /*
  * Test Screens
  */
@@ -96,13 +56,15 @@ $(document).ready(function() {
         loginout: $('#drop-loginout')
     };
 
-    var menu = new DropdownMenu(menuElements, gameManager);
+    var UserMenu = require('UserMenu');
+    var menu = new UserMenu(menuElements, gameManager);
 
     var userMenuElements = {
         activate: $('#user-menu'),
         loginout: $('#drop-loginout')
     };
 
+    var UserMenu = require('UserMenu');
     var userMenu = new UserMenu(userMenuElements);
 
     /**
@@ -112,6 +74,7 @@ $(document).ready(function() {
      */
 
     // singleton game manager
+    var GameManager = require('GameManager');
     var gameManager = GameManager.getGameManager();
 
     console.log('%cTesting GameManager', 'color: #c9a0dc; font-size: x-large');
@@ -120,6 +83,7 @@ $(document).ready(function() {
 
     // temporary fix Only
     var canvasElement = $('#gameplay-canvas').length != 0 ? $('#gameplay-canvas') : $('#editor-canvas');
+    var PixiCanvas = require('pixi-canvas');
     var canvas = new PixiCanvas(canvasElement, 'medium');
     var canvas2 = new PixiCanvas(canvasElement, 'medium');
 });
