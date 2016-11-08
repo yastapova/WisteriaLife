e0862d082bbed33727297e75b3dddc0bed59d4ca
Wendy <wizhacks@github.com>
2016-11-08 00:17:57 -0500

    Update storage rules to allow reading of images. Update enter region text on map screem to be region name.

--- app/js/screens/GameMapScreen.js
+++ app/js/screens/GameMapScreen.js
@@ -1,31 +1,86 @@
 /**
- * gameMap.js
+ * GameMapScreen.js
  * Map region select screen
  */
 var screen = require('./Screen');
+var firebase = require("firebase");
+var gameManager = require('../backend/GameManager');
 
-var gameMap = function (id) {
+var GameMapScreen = function (id) {
     screen.call(this, id);
 };
 
-inherits(gameMap, screen);
+inherits(GameMapScreen, screen);
 
 /**
  * Load available levels/regions based on user progress
  */
-gameMap.prototype.loadAvailableLevels = function () {
+GameMapScreen.prototype.loadAvailableLevels = function () {
 
 };
 
 /*
  * Override the load and hide of the parent screen
  */
-gameMap.prototype.init = function () {
+GameMapScreen.prototype.init = function () {
     console.log("Game map init called!");
+    // update current shape
+    this.loadRegionImages();
 };
 
-gameMap.prototype.hide = function () {
+GameMapScreen.prototype.hide = function () {
 
 };
 
-module.exports = gameMap;
+GameMapScreen.prototype.loadRegionImages = function () {
+	var regionsMap = require('GameManager').levelManager.regionsMap;
+	console.log(require('GameManager'));
+	// Reference to the /images/ storage path
+	var pathReference1 = firebase.storage().ref('img/regions/' + regionsMap.get("The Revolting River").img);
+	var pathReference2 = firebase.storage().ref('img/regions/' + regionsMap.get("The Putrid Plateau").img);
+	var pathReference3 = firebase.storage().ref('img/regions/' + regionsMap.get("The Malignant Marsh").img);
+	var pathReference4 = firebase.storage().ref('img/regions/' + regionsMap.get("The Abysmal Abyss").img);
+	// Get the download URL
+	pathReference1.getDownloadURL().then(function(url) {
+	  $('#region-1-card').css({
+		   "background" : "url(" + url + ") no-repeat center center",
+		   "background-size" : "cover"
+		 });
+	});
+	pathReference2.getDownloadURL().then(function(url) {
+	  $('#region-2-card').css({
+		   "background" : "url(" + url + ") no-repeat center center",
+		   "background-size" : "cover"
+		 });
+	});
+	pathReference3.getDownloadURL().then(function(url) {
+	  $('#region-3-card').css({
+		   "background" : "url(" + url + ") no-repeat center center",
+		   "background-size" : "cover"
+		 });
+	});
+	pathReference4.getDownloadURL().then(function(url) {
+	  $('#region-4-card').css({
+		   "background" : "url(" + url + ") no-repeat center center",
+		   "background-size" : "cover"
+		 });
+	});
+};
+
+// A loading image URL.
+GameMapScreen.LOADING_IMAGE_URL = 'https://www.google.com/images/spin-32.gif';
+
+// Sets the URL of the given img element with the URL of the image stored in Firebase Storage.
+GameMapScreen.prototype.setImageUrl = function(imageUri, imgElement) {
+  // If the image is a Firebase Storage URI we fetch the URL.
+  if (imageUri.startsWith('gs://')) {
+    imgElement.src = GameMapScreen.LOADING_IMAGE_URL; // Display a loading image first.
+    this.storage.refFromURL(imageUri).getMetadata().then(function(metadata) {
+      imgElement.src = metadata.downloadURLs[0];
+    });
+  } else {
+    imgElement.src = imageUri;
+  }
+};
+
+module.exports = GameMapScreen;
