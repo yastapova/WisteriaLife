735be24087e4ccd3f2d180e90bdc7b42d7b0c486
Wendy <wizhacks@github.com>
2016-11-20 14:26:52 -0500

    Finished adding the output ranges. Work on uploading img next.

--- app/js/screens/SaveLevelScreen.js
+++ app/js/screens/SaveLevelScreen.js
@@ -19,16 +19,50 @@ inherits(SaveLevelScreen, Screen);
  */
 SaveLevelScreen.prototype.init = function() {
     console.log("Save levels screen init called");
-
+    // Go back to Level Edit Screen
     $('#save-cancel-button').on('click', function () {
         var gameManager = require('GameManager');
         gameManager.screenManager.hideScreen(this);
     }.bind(this));
+    // Output values for the ranges of the units
     $('#tower_num').on("change", function() {
-    $('.output').val(" x" + this.value);
+    $('.output_tower').val(" x" + this.value);
     }).trigger("change");
     $('#blockade_num').on("change", function() {
-    $('.output').val(" x" + this.value);
+    $('.output_blockade').val(" x" + this.value);
+    }).trigger("change");
+    $('#caltrops_num').on("change", function() {
+    $('.output_caltrops').val(" x" + this.value);
+    }).trigger("change");
+    $('#archernw_num').on("change", function() {
+    $('.output_archernw').val(" x" + this.value);
+    }).trigger("change");
+    $('#archerne_num').on("change", function() {
+    $('.output_archerne').val(" x" + this.value);
+    }).trigger("change");
+    $('#archerse_num').on("change", function() {
+    $('.output_archerse').val(" x" + this.value);
+    }).trigger("change");
+    $('#archersw_num').on("change", function() {
+    $('.output_archersw').val(" x" + this.value);
+    }).trigger("change");
+    $('#infantryw_num').on("change", function() {
+    $('.output_infantryw').val(" x" + this.value);
+    }).trigger("change");
+    $('#infantryn_num').on("change", function() {
+    $('.output_infantryn').val(" x" + this.value);
+    }).trigger("change");
+    $('#infantrye_num').on("change", function() {
+    $('.output_infantrye').val(" x" + this.value);
+    }).trigger("change");
+    $('#infantrys_num').on("change", function() {
+    $('.output_infantrys').val(" x" + this.value);
+    }).trigger("change");
+    $('#wallbackward_num').on("change", function() {
+    $('.output_wallbackward').val(" x" + this.value);
+    }).trigger("change");
+    $('#wallforward_num').on("change", function() {
+    $('.output_wallforward').val(" x" + this.value);
     }).trigger("change");
 };
 
