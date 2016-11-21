6d51776ad3d4e42c9c7a473c390fcbad2e9b0445
Wendy <wizhacks@github.com>
2016-11-21 15:03:23 -0500

    Work on trying to delete levels from fb.

--- app/js/screens/PrivateCustomLevelsScreen.js
+++ app/js/screens/PrivateCustomLevelsScreen.js
@@ -1,9 +1,9 @@
-
 var Screen = require('./Screen');
+var firebase = require('firebase');
 
 var PrivateCustomLevelsScreen = function (id) {
+    this.gameManager = require('GameManager');
     Screen.call(this, id);
-
 };
 
 inherits(PrivateCustomLevelsScreen, Screen);
@@ -14,6 +14,29 @@ PrivateCustomLevelsScreen.prototype.init = function () {
         this.gameManager.user.levels,
         this.setCustomLevels.bind(this)
     );
+
+    // test deleting levels
+    $('#delete-custom-level').on('click', this.deleteLevels);
+};
+
+PrivateCustomLevelsScreen.prototype.deleteLevels = function(){
+	// Get current user
+    var userId = firebase.auth().currentUser.uid;
+	// Grab the ids for the levels that are checked
+	var levels = [];
+	for(var level in levels){
+		// delete from 3 locations: customLevels, levels, and users
+		firebase.database().ref('/customLevels/' + level).remove();
+		firebase.database().ref('/levels/' + level).remove();
+		for(var [key, value] in Object.entries(firebase.database().ref('/users/' + userId + "/levels/"))){
+			if(value === level){
+				firebase.database().ref('/users/' + userId + "/" + key).remove();
+				break;
+			}
+		}
+		// delete img from storage if it exists
+		//firebase.storage().ref
+	}
 };
 
 /**
