74cb31dbe366aac5774fecb856972dc88b6bd7c3
Yuliya Astapova <ellemandora@gmail.com>
2016-12-06 13:36:26 -0500

    fixed enemy spawn placement bug

--- app/js/backend/LevelEditManager.js
+++ app/js/backend/LevelEditManager.js
@@ -379,7 +379,7 @@ LevelEditManager.prototype.placeShape = function(clickRow, clickCol, faction, sh
             return;
         }
         var current_spawns = this.checkForSpawns();
-        if(current_spawns.length >= 5) {
+        if(current_spawns !== undefined && current_spawns.length >= 5) {
             Materialize.toast(
                 'Only 5 enemy spawns allowed per time slot.',
                 2000,
