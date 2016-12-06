e46c3cab64b305463fdaad838a14aa4dff24a60b
Brian Yang <brian@brianyang.me>
2016-12-06 02:29:16 -0500

    Clean up code for level editor - Change selected faction to use class instead (:focus gets unfocused too easily) - Move saving selections on time change to the manager - Combine event handlers for faction selection - Fix favicon path

--- app/js/backend/LevelEditManager.js
+++ app/js/backend/LevelEditManager.js
@@ -193,13 +193,20 @@ LevelEditManager.prototype.changeCurrentTime = function(newTime) {
         this.nonGhostGrid[i] = this.BLANK;
     }
 
+	// save current selections
+	var selectedUnit = this.selectedUnit;
+	var selectedFaction = this.selectedFaction;
+
     this.placeDefenses(this.defenses, false);
     var spawns = this.checkForSpawns();
-    this.selectedUnit = "";
+
     this.spawnEnemies(spawns, false);
-    this.selectedUnit = undefined;
 
     this.renderGridCells();
+
+	// restore current selections
+	this.selectedUnit = selectedUnit;
+	this.selectedFaction = selectedFaction;
 }
 
 /**
@@ -288,7 +295,7 @@ LevelEditManager.prototype.placeShape = function(clickRow, clickCol, faction, sh
         );
         return;
     }
-    if(this.selectedFaction === this.BLANK && 
+    if(this.selectedFaction === this.BLANK &&
        this.selectedUnit !== undefined &&
        this.selectedUnit.name === "void" &&
        faction !== this.GHOST)
@@ -298,7 +305,7 @@ LevelEditManager.prototype.placeShape = function(clickRow, clickCol, faction, sh
     if(addToMaps === undefined && faction !== this.GHOST)
         addToMaps = true;
 
-    if(this.selectedFaction === this.BLANK && 
+    if(this.selectedFaction === this.BLANK &&
        faction === this.BLANK)
     {
         if(addToMaps && this.selectedUnit.name !== "void") {
