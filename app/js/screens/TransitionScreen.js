dd090c37a799d094d20272bae43ebcbbb92b0f0a
Yael Romero <romeroyael@gmail.com>
2016-12-17 22:44:19 -0500

    Random ads

--- app/js/screens/TransitionScreen.js
+++ app/js/screens/TransitionScreen.js
@@ -27,6 +27,18 @@ inherits(TransitionScreen, Screen);
 TransitionScreen.prototype.init = function() {
     console.log("Transition screen init called");
 
+    var transitions = ["/img/transitions/AwesomeLife.png", "/img/transitions/CanaryLife.png", "/img/transitions/FireBrickLife.png",
+    		   "/img/transitions/IKBLife.png", "/img/transitions/PurpleLife.png", "/img/transitions/SalmonLife.png",
+    		   "/img/transitions/ScarletLife.png", "/img/transitions/TimberwolfLife.png"];
+
+    var urls = ["http://awesome-life-game.appspot.com", "http://canary-life.firebaseapp.com", "http://firebrick-life.firebaseapp.com",
+    		    "http://ikb-life.firebaseapp.com", "http://purplelifegame.firebaseapp.com", "http://salmon-life.firebaseapp.com",
+    		    "http://the-scarlet-life.appspot.com", "http://timberwolf-life.firebaseapp.com"];
+
+   	var index = Math.floor(Math.random() * (transitions.length));
+   	$("#transition").attr("src", transitions[index]);
+   	$("#transition-url").attr("href", urls[index]);
+
 };
 
 module.exports = TransitionScreen;
