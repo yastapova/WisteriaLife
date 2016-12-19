427c7fe7eb4589cc81ab06ae701bf90fa001dc0d
Yael Romero <romeroyael@gmail.com>
2016-12-19 01:35:20 -0500

    Social media completed Facebook only works on deployed site

--- app/js/screens/DefeatScreen.js
+++ app/js/screens/DefeatScreen.js
@@ -8,6 +8,7 @@ var Screen = require('./Screen');
   * construct a DefeatScreen obj with given id
   */
 var DefeatScreen = function (id, level) {
+
 	this.gameManager = require('GameManager');
 	this.level = level;
     Screen.call(this, id, true, level);
@@ -19,8 +20,10 @@ inherits(DefeatScreen, Screen);
  * Override the load and hide of the parent screen
  */
 DefeatScreen.prototype.init = function() {
+    
+    $(".dropdown-button").dropdown();
     console.log("Defeat screen init called");
-    document.getElementById('share').onclick = function() {
+    document.getElementById('facebook-share').onclick = function() {
 	    FB.ui({
 	    method: 'share',
 	    display: 'popup',
